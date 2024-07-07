ecb53e664ab04e0f9970af790012600305dbf9c5
Rafael <rafael.barros@kinvo.com.br>
2024-07-07 15:31:21 -0300

    chore(backend/common): add common module

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
 import { CartModule } from './modules/cart/cart.module';
+import { CommonModule } from './common/common.module';
 
 @Module({
-  imports: [CartModule],
+  imports: [CartModule, CommonModule],
 })
 export class AppModule {}
