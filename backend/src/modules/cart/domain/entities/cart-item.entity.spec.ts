3c864dc9b0ec10d3ec47456d9b85a57e03a8751d
Rafael <rafael.barros@kinvo.com.br>
2024-07-08 20:31:04 -0300

    test(back/cart/domain): add cart-item entity

---
+++ backend/src/modules/cart/domain/entities/cart-item.entity.spec.ts
@@ -0,0 +1,76 @@
+import { CartItem } from './cart-item.entity';
+import { Product } from './product.entity';
+import { Quantity } from '../value-objects';
+
+describe('CartItem', () => {
+  let productProps: Product.CreateProps;
+  let quantityProps: Quantity.CreateProps;
+
+  beforeEach(() => {
+    productProps = {
+      id: '25117f80-79f1-4c18-baff-3c726233cb7d',
+      name: 'Floratta',
+      price: BigInt(100),
+    };
+
+    quantityProps = 2;
+  });
+
+  test('should create a CartItem instance', () => {
+    const cartItem = CartItem.create({
+      product: productProps,
+      quantity: quantityProps,
+    });
+
+    expect(cartItem).toBeInstanceOf(CartItem);
+    expect(cartItem.product).toBeInstanceOf(Product);
+    expect(cartItem.quantity).toBeInstanceOf(Quantity);
+    expect(cartItem.product.name).toBe('Floratta');
+    expect(cartItem.quantity.value).toBe(2);
+  });
+
+  test('should increase quantity', () => {
+    const cartItem = CartItem.create({
+      product: productProps,
+      quantity: quantityProps,
+    });
+
+    cartItem.increaseQuantity(2);
+
+    expect(cartItem.quantity.value).toBe(4);
+  });
+
+  test('should decrease quantity', () => {
+    const cartItem = CartItem.create({
+      product: productProps,
+      quantity: quantityProps,
+    });
+
+    cartItem.decreaseQuantity(1);
+
+    expect(cartItem.quantity.value).toBe(1);
+  });
+
+  test('should decrease quantity by default amount', () => {
+    const cartItem = CartItem.create({
+      product: productProps,
+      quantity: quantityProps,
+    });
+
+    cartItem.decreaseQuantity();
+
+    expect(cartItem.quantity.value).toBe(1);
+  });
+
+  test('should convert to JSON', () => {
+    const cartItem = CartItem.create({
+      product: productProps,
+      quantity: quantityProps,
+    });
+
+    const json = cartItem.toJSON();
+
+    expect(json.product.name).toBe('Floratta');
+    expect(json.quantity.value).toBe(2);
+  });
+});
