d83c6de0756bea91769a9f30b7cc1caa26652688
Rafael <rafael.barros@kinvo.com.br>
2024-07-07 23:38:45 -0300

    test(back/cart/domain): add product entity

---
+++ backend/src/modules/cart/domain/entities/product.entity.spec.ts
@@ -0,0 +1,13 @@
+import { Product } from './product.entity';
+
+describe('Product', () => {
+  test('should create a Product instance with correct properties', () => {
+    const price = BigInt(100);
+    const productName = 'Floratta';
+    const product = Product.create({ name: productName, price });
+
+    expect(product).toBeInstanceOf(Product);
+    expect(product.name).toEqual(productName);
+    expect(product.price).toBe(price);
+  });
+});
