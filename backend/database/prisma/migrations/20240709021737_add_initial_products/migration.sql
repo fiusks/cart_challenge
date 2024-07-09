d56de0112e8f698c28b0c15e58c59a0a29c71c1e
Rafael <rafael.barros@kinvo.com.br>
2024-07-08 23:26:35 -0300

    chore(back/database): populate initial products

---
+++ backend/database/prisma/migrations/20240709021737_add_initial_products/migration.sql
@@ -0,0 +1,5 @@
+INSERT INTO "Product" ("id", "name", "price")
+VALUES
+  ('f73df064-302c-4f0a-80ae-1a8f1abafdd1', 'Floratta', 60000000),
+  ('7f1b8f21-3be0-4a36-8561-6a5f56e49f3f', 'Malbec', 210000000),
+  ('b6d8e9a0-3a17-4788-bfa7-6b15ebc9e864', 'Her Code', 150000000);
