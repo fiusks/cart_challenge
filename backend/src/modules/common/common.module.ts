866821058c71a6dfaaab302991bd6e58ad86019c
Rafael <rafael.barros@kinvo.com.br>
2024-07-09 00:28:26 -0300

    chore(back/common): add prismaService and ConfigService as providers and export

--- backend/src/modules/common/common.module.ts
+++ backend/src/modules/common/common.module.ts
@@ -1,4 +1,9 @@
 import { Module } from '@nestjs/common';
+import { PrismaService } from './infra';
+import { ConfigService } from '@nestjs/config';
 
-@Module({})
+@Module({
+  providers: [PrismaService, ConfigService],
+  exports: [PrismaService, ConfigService],
+})
 export class CommonModule {}
