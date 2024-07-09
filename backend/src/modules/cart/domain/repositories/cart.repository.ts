dbc476a945c114158825cb36bb3a7a7fcf482b18
Rafael <rafael.barros@kinvo.com.br>
2024-07-09 00:59:18 -0300

    feat(back/cart/domain): add create to CartRepository

--- backend/src/modules/cart/domain/repositories/cart.repository.ts
+++ backend/src/modules/cart/domain/repositories/cart.repository.ts
@@ -1,6 +1,7 @@
 import { Cart } from '~/modules/cart/domain';
 
 export interface CartRepository {
+  create(cart: Cart): Promise<Cart>;
   findById(id: string): Promise<Cart>;
   update(cart: Cart): Promise<Cart>;
   delete(cart: Cart): Promise<void>;
