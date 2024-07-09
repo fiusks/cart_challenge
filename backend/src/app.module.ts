9d31a89590ae663f8cbd79644b3f6482e5827c04
Rafael <rafael.barros@kinvo.com.br>
2024-07-09 00:40:02 -0300

    chore(back): add global ConfigModule

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,8 +2,18 @@ import { Module } from '@nestjs/common';
 import { CartModule } from './modules/cart/cart.module';
 import { CommonModule } from './modules/common/common.module';
 import { ProductModule } from './modules/product/product.module';
+import { ConfigModule } from '@nestjs/config';
+import { env } from '~/config/env';
 
 @Module({
-  imports: [CartModule, CommonModule, ProductModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validate: env.parse,
+    }),
+    CartModule,
+    CommonModule,
+    ProductModule,
+  ],
 })
 export class AppModule {}
