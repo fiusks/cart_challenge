d77ed5e0a10dcf0ca1b506b4e0ee46ad56ec4b81
Rafael <rafael.barros@kinvo.com.br>
2024-07-08 23:03:04 -0300

    feat(back/common/infra): add prisma service

---
+++ backend/src/modules/common/infra/persistence/prisma/services/prisma.service.ts
@@ -0,0 +1,10 @@
+import { Injectable, OnModuleInit } from '@nestjs/common';
+
+import { PrismaClient } from '@prisma/client';
+
+@Injectable()
+export class PrismaService extends PrismaClient implements OnModuleInit {
+  public async onModuleInit(): Promise<void> {
+    await this.$connect();
+  }
+}
