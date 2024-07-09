b2bf185d861cb3e93240f42028a016c188fc41b6
Rafael <rafael.barros@kinvo.com.br>
2024-07-08 22:30:52 -0300

    refactor(back/common/domain): add create props as optional on EntityId value object

--- backend/src/common/domain/value-objects/entity-id.vo.ts
+++ backend/src/common/domain/value-objects/entity-id.vo.ts
@@ -2,7 +2,7 @@ import { v4 as uuidv4 } from 'uuid';
 import { z } from 'zod';
 
 export class EntityId {
-  public static create(createProps: EntityId.CreateProps): EntityId {
+  public static create(createProps?: EntityId.CreateProps): EntityId {
     if (!createProps) {
       return this.generate();
     }
