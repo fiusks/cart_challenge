6763fe9dcb122e8ddab23769724c65d7f67ab169
Rafael <rafael.barros@kinvo.com.br>
2024-07-10 07:01:06 -0300

    refactor(back/cart/application): create an empty cart on CreateCartService

--- backend/src/modules/cart/application/services/create-cart.service.ts
+++ backend/src/modules/cart/application/services/create-cart.service.ts
@@ -1,11 +1,14 @@
 import { Injectable } from '@nestjs/common';
 import { Cart, CreateCart } from '../../domain';
+import { v4 as uuidv4 } from 'uuid';
 
 @Injectable()
 export class CreateCartService {
   constructor(private readonly createCart: CreateCart) {}
 
-  public async execute(cart: Cart): Promise<Cart> {
-    return this.createCart.execute(cart);
+  public async execute(): Promise<Cart> {
+    return this.createCart.execute(
+      Cart.create({ sessionId: uuidv4(), items: [] }),
+    );
   }
 }
