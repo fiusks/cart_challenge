1184d90a4ba680f2277ddad3294dee3abac7590c
Rafael <rafael.barros@kinvo.com.br>
2024-07-10 08:45:41 -0300

    refactor(back/product/domain): return null if product was not found

--- backend/src/modules/product/domain/repositories/product.repository.ts
+++ backend/src/modules/product/domain/repositories/product.repository.ts
@@ -1,6 +1,6 @@
 import { Product } from '../entities';
 
 export interface ProductRepository {
-  findById(id: string): Promise<Product>;
+  findById(id: string): Promise<Product | null>;
   findAll(): Promise<Product[]>;
 }
