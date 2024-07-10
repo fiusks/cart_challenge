1dbde802ec1661da78f121d5c0038bfd91647041
Rafael <rafael.barros@kinvo.com.br>
2024-07-10 13:23:04 -0300

    chore(front): setup nextJS initial project

---
+++ frontend/src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main className="flex-1 flex-col items-center justify-between bg-neutral-50 rounded-lg">
+      <p>Hello World</p>
+    </main>
+  );
+}
