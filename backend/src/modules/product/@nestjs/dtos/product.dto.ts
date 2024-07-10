d1ea5d07f9565e4a417fcdbfec3b18f041fe8c3e
Rafael <rafael.barros@kinvo.com.br>
2024-07-10 06:49:30 -0300

    feat(back/product/@nestjs): add productDto

---
+++ backend/src/modules/product/@nestjs/dtos/product.dto.ts
@@ -0,0 +1,7 @@
+export class ProductDto {
+  public readonly id: string;
+  public readonly name: string;
+  public readonly price: string;
+  public readonly createdAt: Date;
+  public readonly updatedAt: Date;
+}
