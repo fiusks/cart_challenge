52469595f783503c88ed6df4638a5164444934b9
Rafael <rafael.barros@kinvo.com.br>
2024-07-10 08:58:31 -0300

    feat(back/cart/domain): add RemoveCartItem domain service

--- backend/src/modules/cart/domain/services/index.ts
+++ backend/src/modules/cart/domain/services/index.ts
@@ -2,4 +2,5 @@ export * from './add-cart-item';
 export * from './create-cart';
 export * from './delete-cart';
 export * from './find-cart-by-id';
+export * from './remove-cart-item';
 export * from './update-cart';
