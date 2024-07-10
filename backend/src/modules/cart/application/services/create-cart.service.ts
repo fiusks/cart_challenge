c93e4cf4e365845e7b3c17f0b25dab352a69eb19
Rafael <rafael.barros@kinvo.com.br>
2024-07-10 08:12:49 -0300

    refactor(back/cart/application): pass sessionId as parameter to create new cart

--- backend/src/modules/cart/application/services/create-cart.service.ts
+++ backend/src/modules/cart/application/services/create-cart.service.ts
@@ -1,14 +1,11 @@
 import { Injectable } from '@nestjs/common';
 import { Cart, CreateCart } from '../../domain';
-import { v4 as uuidv4 } from 'uuid';
 
 @Injectable()
 export class CreateCartService {
   constructor(private readonly createCart: CreateCart) {}
 
-  public async execute(): Promise<Cart> {
-    return this.createCart.execute(
-      Cart.create({ sessionId: uuidv4(), items: [] }),
-    );
+  public async execute(sessionId: string): Promise<Cart> {
+    return this.createCart.execute(Cart.create({ sessionId, items: [] }));
   }
 }
