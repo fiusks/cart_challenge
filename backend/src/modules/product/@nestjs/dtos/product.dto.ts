cbf03426a8934ccd53f3301c291725547786ce4e
Rafael <rafael.barros@kinvo.com.br>
2024-07-10 18:48:52 -0300

    feat(back/product/@nestjs): add imageUrl to productDto

--- backend/src/modules/product/@nestjs/dtos/product.dto.ts
+++ backend/src/modules/product/@nestjs/dtos/product.dto.ts
@@ -2,6 +2,7 @@ export class ProductDto {
   public readonly id: string;
   public readonly name: string;
   public readonly price: string;
+  public readonly imageUrl: string;
   public readonly createdAt: Date;
   public readonly updatedAt: Date;
 }
