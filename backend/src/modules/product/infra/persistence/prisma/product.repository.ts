50b3468ebf87d42f2548c5c22aad1d319594d8d9
Rafael <rafael.barros@kinvo.com.br>
2024-07-10 08:45:51 -0300

    refactor(back/product/infra): return null if product was not found

--- backend/src/modules/product/infra/persistence/prisma/product.repository.ts
+++ backend/src/modules/product/infra/persistence/prisma/product.repository.ts
@@ -1,16 +1,15 @@
-import { NotFoundException } from '@nestjs/common';
 import { PrismaService } from '~/modules/common/infra';
 import { Product, ProductRepository } from '~/modules/product/domain';
 
 export class PrismaProductRepository implements ProductRepository {
   constructor(private readonly prisma: PrismaService) {}
 
-  public async findById(id: string): Promise<Product> {
+  public async findById(id: string): Promise<Product | null> {
     const prismaProduct = await this.prisma.product.findUnique({
       where: { id },
     });
 
-    if (!prismaProduct) throw new NotFoundException('Produto não encontrado');
+    if (!prismaProduct) return null;
 
     return Product.create(prismaProduct);
   }
