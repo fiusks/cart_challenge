44a039407b2d3e7903b160bc36dd5533562851f5
Rafael <rafael.barros@kinvo.com.br>
2024-07-10 13:03:30 -0300

    feat(back/cart/domain): add cart-item handle methods to repository

--- backend/src/modules/cart/domain/repositories/cart.repository.ts
+++ backend/src/modules/cart/domain/repositories/cart.repository.ts
@@ -1,8 +1,11 @@
-import { Cart } from '~/modules/cart/domain';
+import { Cart, CartItem } from '~/modules/cart/domain';
 
 export interface CartRepository {
   create(cart: Cart): Promise<Cart>;
   findById(id: string): Promise<Cart | null>;
+  addCartItem(sessionId: string, cartItem: CartItem): Promise<Cart>;
+  removeCartItem(sessionId: string, cartItem: CartItem): Promise<Cart>;
+  deleteCartItem(sessionId: string, cartItemId: string): Promise<Cart>;
   update(cart: Cart): Promise<Cart>;
   delete(cart: Cart): Promise<void>;
 }
