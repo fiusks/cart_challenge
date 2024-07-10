038677f4e81cd8d20a36966122d77daf5cf782d4
Rafael <rafael.barros@kinvo.com.br>
2024-07-10 08:27:24 -0300

    feat(back/cart/@nestjs): add Get Carts session to CartController

--- backend/src/modules/cart/@nestjs/controller/cart.controller.ts
+++ backend/src/modules/cart/@nestjs/controller/cart.controller.ts
@@ -1,15 +1,25 @@
 import { Controller, Get, Param, Post } from '@nestjs/common';
-import { CreateCartService } from '../../application';
+import { CreateCartService, FindCartByIdService } from '../../application';
 import { plainToInstance } from 'class-transformer';
 import { CartDto } from '../dto/cart.dto';
 
 @Controller()
 export class CartController {
-  constructor(private readonly createCart: CreateCartService) {}
+  constructor(
+    private readonly createCart: CreateCartService,
+    private readonly findCartById: FindCartByIdService,
+  ) {}
 
   @Post('/carts/:sessionId')
-  public async index(@Param('sessionId') sessionId: string): Promise<any> {
+  public async store(@Param('sessionId') sessionId: string): Promise<any> {
     const cart = await this.createCart.execute(sessionId);
+
+    return plainToInstance(CartDto, cart.toJSON());
+  }
+
+  @Get('/carts/:sessionId')
+  public async index(@Param('sessionId') sessionId: string): Promise<any> {
+    const cart = await this.findCartById.execute(sessionId);
 
     return plainToInstance(CartDto, cart.toJSON());
   }
