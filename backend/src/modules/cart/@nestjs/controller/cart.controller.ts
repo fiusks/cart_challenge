ce040a3c58b6265e2433961e70348116f96aa1ad
Rafael <rafael.barros@kinvo.com.br>
2024-07-10 13:09:49 -0300

    feat(back/cart/@nestjs): add AddCartItem and RemoveCartItem to Cart Controller

--- backend/src/modules/cart/@nestjs/controller/cart.controller.ts
+++ backend/src/modules/cart/@nestjs/controller/cart.controller.ts
@@ -1,5 +1,17 @@
-import { Controller, Get, Param, Post } from '@nestjs/common';
-import { CreateCartService, FindCartByIdService } from '../../application';
+import {
+  Controller,
+  Delete,
+  Get,
+  Param,
+  ParseIntPipe,
+  Post,
+} from '@nestjs/common';
+import {
+  AddCartItemService,
+  CreateCartService,
+  FindCartByIdService,
+  RemoveCartItemService,
+} from '../../application';
 import { plainToInstance } from 'class-transformer';
 import { CartDto } from '../dto/cart.dto';
 
@@ -8,17 +20,49 @@ export class CartController {
   constructor(
     private readonly createCart: CreateCartService,
     private readonly findCartById: FindCartByIdService,
+    private readonly addCartItem: AddCartItemService,
+    private readonly removeCartItem: RemoveCartItemService,
   ) {}
 
   @Post('/carts/:sessionId')
-  public async store(@Param('sessionId') sessionId: string): Promise<any> {
+  public async store(@Param('sessionId') sessionId: string): Promise<CartDto> {
     const cart = await this.createCart.execute(sessionId);
 
     return plainToInstance(CartDto, cart.toJSON());
   }
 
+  @Post('/carts/:sessionId/:productId/:quantity')
+  public async addItem(
+    @Param('sessionId') sessionId: string,
+    @Param('productId') productId: string,
+    @Param('quantity', ParseIntPipe) quantity: number,
+  ): Promise<CartDto> {
+    const cart = await this.addCartItem.execute({
+      sessionId,
+      productId,
+      quantity,
+    });
+
+    return plainToInstance(CartDto, cart.toJSON());
+  }
+
+  @Delete('/carts/:sessionId/:productId/:quantity')
+  public async removeItem(
+    @Param('sessionId') sessionId: string,
+    @Param('productId') productId: string,
+    @Param('quantity', ParseIntPipe) quantity: number,
+  ): Promise<CartDto> {
+    const cart = await this.removeCartItem.execute({
+      sessionId,
+      productId,
+      quantity,
+    });
+
+    return plainToInstance(CartDto, cart.toJSON());
+  }
+
   @Get('/carts/:sessionId')
-  public async index(@Param('sessionId') sessionId: string): Promise<any> {
+  public async index(@Param('sessionId') sessionId: string): Promise<CartDto> {
     const cart = await this.findCartById.execute(sessionId);
 
     return plainToInstance(CartDto, cart.toJSON());
