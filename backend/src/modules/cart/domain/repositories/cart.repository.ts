61d7b11ab97855ae73c96494a735a5f1b035ad1e
Rafael <rafael.barros@kinvo.com.br>
2024-07-10 08:17:14 -0300

    refactor(back/cart/domain): update findById to return null

--- backend/src/modules/cart/domain/repositories/cart.repository.ts
+++ backend/src/modules/cart/domain/repositories/cart.repository.ts
@@ -2,7 +2,7 @@ import { Cart } from '~/modules/cart/domain';
 
 export interface CartRepository {
   create(cart: Cart): Promise<Cart>;
-  findById(id: string): Promise<Cart>;
+  findById(id: string): Promise<Cart | null>;
   update(cart: Cart): Promise<Cart>;
   delete(cart: Cart): Promise<void>;
 }
