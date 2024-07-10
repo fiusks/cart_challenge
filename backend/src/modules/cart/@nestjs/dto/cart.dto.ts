6318cf0f9961da4f0e46fd24f534f9ce167131bf
Rafael <rafael.barros@kinvo.com.br>
2024-07-10 06:59:30 -0300

    feat(back/cart/@nestjs): add cart Dto

---
+++ backend/src/modules/cart/@nestjs/dto/cart.dto.ts
@@ -0,0 +1,15 @@
+import { Type } from 'class-transformer';
+import { CartItemDto } from './cart-item.dto';
+
+export class CartDto {
+  public readonly id: string;
+  public readonly sessionId: string;
+
+  @Type(() => CartItemDto)
+  public readonly items: CartItemDto[];
+
+  public readonly total: string;
+
+  public readonly createdAt: Date;
+  public readonly updatedAt: Date;
+}
