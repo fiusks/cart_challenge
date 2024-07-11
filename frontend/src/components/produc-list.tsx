f2f99bae31359bb373a14ace11009f724398d5ab
Rafael <rafael.barros@kinvo.com.br>
2024-07-11 20:11:44 -0300

    feat(front/components): add handleAddProduct to product list

--- frontend/src/components/produc-list.tsx
+++ frontend/src/components/produc-list.tsx
@@ -1,14 +1,16 @@
+'use client';
+
+import { handleAddProduct } from '@/app/actions';
 import { convertCurrency, Product } from '@/modules';
 import Image from 'next/image';
 
 interface ProductListPorps {
   products: Product[];
-  addToCart: (producId: string) => Promise<void>;
 }
 
-export async function ProductsList({ products, addToCart }: ProductListPorps) {
+export async function ProductsList({ products }: ProductListPorps) {
   return (
-    <section className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4 items-center p-8">
+    <section className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 items-center p-8">
       {products.map((product) => (
         <div
           key={product.id}
@@ -28,6 +30,7 @@ export async function ProductsList({ products, addToCart }: ProductListPorps) {
             </p>
 
             <button
+              onClick={() => handleAddProduct(product.id)}
               type="submit"
               className="bg-blue-500 text-white rounded-md p-2 mt-4 hover:bg-blue-600 transition duration-200 ease-in-out text-xs"
             >
