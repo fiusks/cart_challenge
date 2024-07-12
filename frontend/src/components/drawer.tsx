d454768ffd6660ce6d3e075e38028f1d7c2127ab
Rafael <rafael.barros@kinvo.com.br>
2024-07-12 12:45:32 -0300

    refactor(front/components): add position to drawer properties

--- frontend/src/components/drawer.tsx
+++ frontend/src/components/drawer.tsx
@@ -1,18 +1,26 @@
-// components/drawer.tsx
 import { ReactElement, ReactNode } from 'react';
 
 interface DrawerProps {
   onClose: () => void;
   children: ReactNode;
-  title: string;
-  icon?: ReactElement;
+  title?: string;
+  position?: string;
+  icon?: ReactNode;
 }
 
-const Drawer = ({ onClose, children, title, icon }: DrawerProps) => {
+const Drawer = ({
+  onClose,
+  children,
+  title,
+  icon,
+  position = 'right',
+}: DrawerProps) => {
+  const positionClass = position === 'left' ? 'left-0' : 'right-0';
+
   return (
     <div
-      className={`fixed inset-y-0 right-0 transform 'translate-x-0' : 'translate-x-full'
-      transition-transform duration-300 ease-in-out w-[340px] bg-white shadow-lg z-50`}
+      className={`fixed inset-y-0 ${positionClass} transform transition-transform duration-300 ease-in-out
+      translate-x-0 w-full sm:w-1/2 md:w-1/3 lg:w-25 bg-white shadow-lg z-50`}
     >
       <div className="p-4 space-x-5 justify-between text-center flex w-full">
         <div className="flex items-center space-x-2">
