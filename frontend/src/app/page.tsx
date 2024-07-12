d8dfb5ce3fd47f2571504e0bee73a9276085abf3
Rafael <rafael.barros@kinvo.com.br>
2024-07-12 17:22:54 -0300

    refactor(front/app): refactor main page data fetching

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,42 +1,30 @@
 import { ProductsList } from '@/components/produc-list';
 import { Header } from '../components/header';
-import { makeGetProducts, Product } from '@/modules';
-import {
-  makeAddCartItem,
-  makeCreateCart,
-  makeGetCart,
-} from '@/modules/cart/main';
+
+import { makeGetProducts } from '@/modules';
+import { handleGetCart } from './actions';
+import { cookies } from 'next/headers';
+import { revalidateTag } from 'next/cache';
 
 export default async function Home() {
-  const sessionId = '9f5b2714-09f1-4b68-9e84-c82779652e49';
-
-  // Fetch products and initial cart
-
-  const initialCart = await (async () => {
-    const existingCart = await makeGetCart().execute(sessionId);
-
-    if (!existingCart) {
-      return await makeCreateCart().execute(sessionId);
-    }
-
-    return existingCart;
-  })();
-
-  async function handleAddProduct(productId: string) {
-    'use server';
-    const updatedCart = await makeAddCartItem().execute({
-      productId,
-      sessionId,
-    });
-    console.log('🚀 ~ updatedCart ~ updatedCart:', updatedCart);
-  }
-
   const products = await makeGetProducts().execute();
 
+  const cart = await (async () => {
+    const sessionId = cookies().get('sessionId');
+
+    if (sessionId) {
+      return await handleGetCart(sessionId.value);
+    }
+
+    revalidateTag('cart');
+
+    return null;
+  })();
+
   return (
-    <main className="flex-1 flex-col items-center justify-between bg-neutral-50 rounded-lg">
-      <Header cartItemsCount={initialCart.items.length} />
-      <ProductsList products={products} addToCart={handleAddProduct} />
+    <main className="flex-1 flex-col items-center justify-between bg-neutral-50 rounded-lg min-h-screen">
+      <Header cart={cart} />
+      <ProductsList products={products} />
     </main>
   );
 }
