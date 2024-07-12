2e30d9091d0c19cf35f4bd4e7a0758983f2f374d
Rafael <rafael.barros@kinvo.com.br>
2024-07-12 19:03:07 -0300

    chore(cart): register DeleteCartOnExpireQueue on module

--- backend/src/modules/cart/cart.module.ts
+++ backend/src/modules/cart/cart.module.ts
@@ -7,9 +7,15 @@ import {
   domainFactories,
   infraFactories,
 } from './@nestjs/factories';
+import { BullModule } from '@nestjs/bull';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [ProductModule],
+  imports: [
+    ProductModule,
+    ConfigModule,
+    BullModule.registerQueue({ name: 'DeleteCartOnExpireQueue' }),
+  ],
   providers: [...domainFactories, ...applicationFactories, ...infraFactories],
   controllers: [CartController],
 })
