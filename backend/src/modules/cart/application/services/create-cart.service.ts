5560b3655ce56ce6e07578321932c680e4c99c0d
Rafael <rafael.barros@kinvo.com.br>
2024-07-12 18:55:52 -0300

    feat(back/cart/application): add job to exclude cart if it was not update after 24h

--- backend/src/modules/cart/application/services/create-cart.service.ts
+++ backend/src/modules/cart/application/services/create-cart.service.ts
@@ -1,11 +1,25 @@
 import { Injectable } from '@nestjs/common';
 import { Cart, CreateCart } from '../../domain';
+import { InjectQueue } from '@nestjs/bull';
+import { Queue } from 'bull';
 
 @Injectable()
 export class CreateCartService {
-  constructor(private readonly createCart: CreateCart) {}
+  constructor(
+    private readonly createCart: CreateCart,
+    @InjectQueue('DeleteCartOnExpireQueue')
+    protected readonly deleteCartQueue: Queue<Cart>,
+  ) {}
 
   public async execute(sessionId: string): Promise<Cart> {
-    return this.createCart.execute(Cart.create({ sessionId, items: [] }));
+    const newCart = await this.createCart.execute(
+      Cart.create({ sessionId, items: [] }),
+    );
+
+    this.deleteCartQueue.add('DeleteCartOnExpireQueue', newCart, {
+      delay: 60 * 60 * 24,
+    });
+
+    return newCart;
   }
 }
