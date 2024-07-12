b7b3851ee38841ef6e8b25db38cbde6649c726c9
Rafael <rafael.barros@kinvo.com.br>
2024-07-12 12:58:17 -0300

    refactor(front/components): change close button position on drawer dynamically

--- frontend/src/components/drawer.tsx
+++ frontend/src/components/drawer.tsx
@@ -1,4 +1,4 @@
-import { ReactElement, ReactNode } from 'react';
+import { ReactNode } from 'react';
 
 interface DrawerProps {
   onClose: () => void;
@@ -15,19 +15,23 @@ const Drawer = ({
   icon,
   position = 'right',
 }: DrawerProps) => {
-  const positionClass = position === 'left' ? 'left-0' : 'right-0';
+  const isLeft = position == 'left';
+  const positionClass = isLeft ? 'left-0' : 'right-0';
+  const closeBtnPosition = isLeft ? 'flex-row-reverse' : '';
 
   return (
     <div
       className={`fixed inset-y-0 ${positionClass} transform transition-transform duration-300 ease-in-out
       translate-x-0 w-full sm:w-1/2 md:w-1/3 lg:w-25 bg-white shadow-lg z-50`}
     >
-      <div className="p-4 space-x-5 justify-between text-center flex w-full">
-        <div className="flex items-center space-x-2">
+      <div
+        className={`p-4 space-x-5 justify-between text-center flex ${closeBtnPosition} w-full`}
+      >
+        <div className="flex items-center  space-x-2">
           <div className="text-xl">{icon}</div>
           <div className="text-sm font-semibold">{title}</div>
         </div>
-        <button className="text-gray-500 text-lg" onClick={onClose}>
+        <button className=" text-gray-500 text-lg" onClick={onClose}>
           &times;
         </button>
       </div>
